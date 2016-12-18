f8b4a9f78de76d5cb967f4f2d268c9ca75c55173
Mostafa Said <mostafamostafa741@hotmail.com>
2016-12-18 20:38:28 +0200

    Add project files.

---
+++ Face_recognition_LBP/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Face_recognition_LBP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.Video = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.GPGPU = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(12, 31);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(895, 596);
+            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // Video
+            // 
+            this.Video.Location = new System.Drawing.Point(157, 653);
+            this.Video.Name = "Video";
+            this.Video.Size = new System.Drawing.Size(75, 23);
+            this.Video.TabIndex = 3;
+            this.Video.Text = "Capture";
+            this.Video.UseVisualStyleBackColor = true;
+            this.Video.Click += new System.EventHandler(this.Video_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1,
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(919, 25);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.loadToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(38, 22);
+            this.toolStripDropDownButton1.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // GPGPU
+            // 
+            this.GPGPU.AutoSize = true;
+            this.GPGPU.Location = new System.Drawing.Point(628, 658);
+            this.GPGPU.Name = "GPGPU";
+            this.GPGPU.Size = new System.Drawing.Size(59, 17);
+            this.GPGPU.TabIndex = 5;
+            this.GPGPU.Text = "GPGPU";
+            this.GPGPU.UseVisualStyleBackColor = true;
+            this.GPGPU.CheckedChanged += new System.EventHandler(this.GPGPU_CheckedChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 662);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "FPS";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(44, 22);
+            this.toolStripButton1.Text = "About";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 713);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GPGPU);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.Video);
+            this.Controls.Add(this.imageBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Face recognition LBP";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button Video;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.CheckBox GPGPU;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+    }
+}
+
---
+++ Face_recognition_LBP/Form1.cs
@@ -0,0 +1,184 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.GPU;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using Emgu.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Face_recognition_LBP
+{
+    public partial class Form1 : Form
+    {
+        #region Variables
+        const string haarcascade = "haarcascade_frontalface_default.xml";
+        const string haarcascade_cuda = "haarcascade_frontalface_default_cuda.xml";
+        bool GPU_classifier_selector = false;
+        int counter, fps;
+        GpuCascadeClassifier GPU_classifier;
+        CascadeClassifier classifier;
+        Capture capture;
+        FaceRecognizer recognizer;
+        Image<Gray, Byte> frame_gray;
+        Image<Bgr, Byte> frame_BGR;
+        Rectangle[] detected_faces;
+        MCvFont font;
+        List<Image<Gray, Byte>> imagesList = new List<Image<Gray, byte>>();
+        List<int> imagesLabels_indices = new List<int>();
+        List<string> imagesLabels = new List<string>();
+        Image<Gray, Byte> temp;
+
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+            recognizer = new LBPHFaceRecognizer(1, 8, 8, 9, 65);
+
+            classifier = new CascadeClassifier(haarcascade);
+            GPU_classifier = new GpuCascadeClassifier(haarcascade_cuda);
+
+            font = new MCvFont(Emgu.CV.CvEnum.FONT.CV_FONT_HERSHEY_TRIPLEX, 0.5, 0.5);
+            if (File.Exists(@"traningdata.xml"))
+            {
+                recognizer.Load(@"traningdata.xml");
+            }
+            else
+            {
+
+                foreach (var file in Directory.GetFiles(Application.StartupPath + @"\Traning Faces\"))
+                {
+                    try { temp = new Image<Gray, Byte>(file); }
+                    catch { continue; }
+                    temp._EqualizeHist();
+
+                    var detectedFaces = classifier.DetectMultiScale(temp, 1.1, 15, new Size(24, 24), Size.Empty);
+                    if (detectedFaces.Length == 0)
+                    {
+                        continue;
+                    }
+
+                    temp.ROI = detectedFaces[0];
+                    temp = temp.Copy();
+                    temp = temp.Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                    imagesList.Add(temp);
+                    imagesLabels.Add(Path.GetFileNameWithoutExtension(file));
+                }
+                for (int i = 0; i < imagesList.Count; i++)
+                {
+                    imagesLabels_indices.Add(i);
+                }
+
+                try { recognizer.Train(imagesList.ToArray(), imagesLabels_indices.ToArray()); }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+        private void Video_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                capture = new Capture(CaptureType.ANY);
+                Video.Enabled = false;
+                timer1.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            Application.Idle += frame_parser;
+        }
+
+        void frame_parser(object sender, EventArgs e)
+        {
+            frame_BGR = capture.QueryFrame();
+            frame_gray = frame_BGR.Convert<Gray, Byte>();
+            imageBox1.Image = LBP_recognizer(frame_BGR, frame_gray);
+            counter++;
+        }
+
+        Image<Bgr, Byte> LBP_recognizer(Image<Bgr, Byte> image_BGR, Image<Gray, Byte> image_gray)
+        {
+            image_BGR = image_BGR.Copy();
+            image_gray = image_gray.Copy();
+            image_gray._EqualizeHist();
+
+
+            if (GPU_classifier_selector)
+            {
+                detected_faces = GPU_classifier.DetectMultiScale(new GpuImage<Gray, Byte>(image_gray), 1.1, 15, new Size(24, 24));
+            }
+            else
+            {
+                detected_faces = classifier.DetectMultiScale(image_gray, 1.1, 15, new Size(24, 24), Size.Empty);
+            }
+
+            foreach (var face in detected_faces)
+            {
+                image_gray.ROI = face;
+                var result = recognizer.Predict(image_gray.Copy().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC));
+                if (result.Label == -1)
+                {
+                    image_BGR.Draw(new Rectangle(face.Location, face.Size), new Bgr(0, 0, 255), 3);
+
+                    image_BGR.Draw("Unknown", ref font, new Point(face.X - 20, face.Y - 20), new Bgr(0, 0, 255));
+                }
+                else
+                {
+                    image_BGR.Draw(new Rectangle(face.Location, face.Size), new Bgr(0, 255, 0), 3);
+
+                    image_BGR.Draw(imagesLabels[result.Label], ref font, new Point(face.X - 20, face.Y - 20), new Bgr(0, 0, 255));
+                }
+                //image_BGR.ROI = image_gray.ROI;
+                image_gray.ROI = Rectangle.Empty;
+            }
+            return image_BGR;
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            recognizer.Save("traningdata.xml");
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            recognizer.Load("traningdata.xml");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void GPGPU_CheckedChanged(object sender, EventArgs e)
+        {
+            GPU_classifier_selector = GPGPU.Checked;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            fps = (counter * 1000) / timer1.Interval;
+            label1.Text = "FPS=" + fps.ToString();
+            counter = 0;
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This program licenced under GPL V3","About",MessageBoxButtons.OK,MessageBoxIcon.Information,MessageBoxDefaultButton.Button1);
+        }
+    }
+}
